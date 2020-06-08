4538a653b07a88f823366b10204f223f1ee2b89f
Ashish <ashish.khanal@hotmail.com>
2020-06-08 21:08:08 +0200

    changes to the notification

--- backend/api/models/notification.js
+++ backend/api/models/notification.js
@@ -2,6 +2,9 @@ const mongoose = require('mongoose');
 
 const notificationSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    userId: {type: mongoose.Schema.Types.ObjectId},
+    eventID:{type: mongoose.Schema.Types.ObjectId},
+    memberId: {type: mongoose.Schema.Types.ObjectId},
     date_created:  {type:Date},
     text: {type:String},
     is_read: {type: Boolean}
