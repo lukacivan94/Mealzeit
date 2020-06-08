aaf95d86e469237d82919b9e8d047f45329838dd
Sabin <sabin.bhandari15@gmail.com>
2020-06-08 21:19:55 +0200

    minor changes in event

--- backend/api/models/event.js
+++ backend/api/models/event.js
@@ -3,18 +3,17 @@ const mongoose = require('mongoose');
 const eventSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     location: { type: String, required: true },
-    date_of_event: {type: Date},
-    time_of_event: {type: Date},
+    date_time: {type: Date},
     date_of_publish: {type: Date},
 
-    list_of_recipe: {type: Array},
+    list_of_recipes: {type: Array},
     course_rating: {type: Number},
     is_virtual: {type: Boolean},
-    price: {type: Number},
+    price_of_course: {type: Number},
     is_included_in_premium: {type: Boolean},
     number_of_participants: {type: Number},
 
-    friends: {type: Array},
+    users: {type: Array},
     instant_join: {type: Boolean},
     description: {type: String},
     is_volunteering: {type: Boolean},
