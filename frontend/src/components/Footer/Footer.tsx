74c77b93efc68ea8e8dac8eab2f3e9b22ae5165b
buraktomruk <btburak@hotmail.com>
2020-06-13 16:50:02 +0300

    Added basic layout and footer components

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.div`
+    background-color: #F88805;
+    border-top: 1px solid #E7E7E7;
+    text-align: center;
+    padding: 20px;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    height: 40px;
+    width: 100%;
+`;
+
+const StyledFooterHeader = styled.div`
+    display: block;
+    padding: 20px;
+    height: 30px;
+    width: 100%;
+`;
+
+const StyledLink = styled.a`
+    padding: 10px;
+
+    color: white;
+    text-decoration: none;
+    text-align: center;
+`;
+
+const StyledList = styled.ul`
+    color: $white;
+
+    display: flex;
+`;
+
+const StyledListItem = styled.li`
+    list-style-type: none;
+`;
+
+const Footer = () => (
+    <div>
+        <StyledFooterHeader />
+        <StyledFooter >
+            <StyledList >
+                <StyledListItem><StyledLink href='/'>About Us</StyledLink></StyledListItem>
+                <StyledListItem><span>-</span></StyledListItem>
+                <StyledListItem><StyledLink href='/'>Team</StyledLink></StyledListItem>
+                <StyledListItem><span>-</span></StyledListItem>
+                <StyledListItem><StyledLink href='/'>Help and Support</StyledLink></StyledListItem>
+                <StyledListItem><span>-</span></StyledListItem>
+                <StyledListItem><StyledLink href='/'>Partnership</StyledLink></StyledListItem>
+            </StyledList>
+            <div>{}</div>
+        </StyledFooter>
+    </div>
+);
+
+export default Footer;
---
+++ frontend/src/components/Layout/Layout.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Layout = (props: any) => (
+    <main>
+        {props.children}
+    </main>
+);
