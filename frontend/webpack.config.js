ce50c99e1dbe57cfc9a36b3478f92d835769829e
Sabin <sabin.bhandari15@gmail.com>
2020-06-19 14:49:34 +0200

    added favicon

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -42,7 +42,8 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './src/index.html'
+            template: './src/index.html',
+            favicon: "./src/assets/images/favicon.ico"
         })
     ]
 };
