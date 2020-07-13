e01409c43aac5224a653c13c0871e2ea2c90d450
Sabin <sabin.bhandari15@gmail.com>
2020-07-13 13:31:42 +0200

    routes fix

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import Screen from '../components/Screen/Screen';
+import SignUp from '../screens/SignUp/SignUp';
 import HomePage from '../screens/Home/HomePage';
 import EventPage from '../screens/Event/EventsPages';
 import Auth from '../screens/Auth/Auth';
@@ -12,7 +11,7 @@ const Routes = () => (
         <Switch>
             <Route exact path='/sign-up' component={SignUp} />
             <Route exact path='/sign-in' component={Auth} />
-            <Route path='/' component={() => <HomePage />} />
+            <Route exact path='/' component={() => <HomePage />} />
             <Route path='/event' component={() => <EventPage />} />
             <Route render={() => <h1>404 Page not found</h1>} />
         </Switch>
