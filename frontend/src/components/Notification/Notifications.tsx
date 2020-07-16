4a39575d077b94cd8137c7d01ad145ea3cfe7360
buraktomruk <btburak@hotmail.com>
2020-07-17 01:00:50 +0300

    Added accept and reject notifications

--- frontend/src/components/Notification/Notifications.tsx
+++ frontend/src/components/Notification/Notifications.tsx
@@ -7,6 +7,8 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import ClearIcon from '@material-ui/icons/Clear';
+import CheckIcon from '@material-ui/icons/Check';
 import MailIcon from '@material-ui/icons/Mail';
 import axios from '../../axios';
 import { Divider, Badge } from '@material-ui/core';
@@ -64,9 +66,9 @@ const Notifications = () => {
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
         const userId = localStorage.getItem('userId');
+
         axios.get('/notifications/unread/' + userId)
             .then(res => {
-                console.log('res: ', res);
                 setNotifications(res.data.notifications);
             })
             .catch(error => {
@@ -83,6 +85,44 @@ const Notifications = () => {
         setNotifications([]);
     };
 
+    const handleAccept = (index) => {
+        const selectedNot = notifications[index];
+        const eventId = selectedNot.eventId;
+        const memberId = selectedNot.memberId;
+        const notId = selectedNot._id;
+
+        axios.patch('/cookrooms/accreq/' + eventId + '/' + memberId)
+            .then((res) => {
+                setNotifications(notifications.filter((item) => item._id !== notId));
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                }
+            });
+    };
+
+    const handleReject = (index) => {
+        const selectedNot = notifications[index];
+        const eventId = selectedNot.eventId;
+        const memberId = selectedNot.memberId;
+        const notId = selectedNot._id;
+
+        axios.patch('/cookrooms/rejectreq/' + eventId + '/' + memberId)
+            .then((res) => {
+                setNotifications(notifications.filter((item) => item._id !== notId));
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.log(error.response.data);
+                    console.log(error.response.status);
+                    console.log(error.response.headers);
+                }
+            });
+    };
+
     return (
         <div className={classes.root}>
             <Badge badgeContent={notifications.length} color='secondary'>
@@ -96,17 +136,27 @@ const Notifications = () => {
                 onClose={handleClose}
             >
                 {
-                    notifications.map((notificationItem, index) => (
-                        <>
-                            <StyledMenuItem key={index}>
+                    notifications && notifications.length > 0 ? notifications.map((notificationItem, index) => (
+                        <div key={index}>
+                            <StyledMenuItem>
                                 <ListItemIcon>
                                     <MailIcon fontSize='small' />
                                 </ListItemIcon>
                                 <ListItemText primary={notificationItem.text} />
+                                {notificationItem.type && notificationItem.type === 'request' &&
+                                    <>
+                                        <CheckIcon onClick={() => handleAccept(index)} style={{ color: 'green', margin: '10px' }} />
+                                        <ClearIcon onClick={() => handleReject(index)} style={{ color: 'red', margin: '10px' }} />
+                                    </>
+                                }
                             </StyledMenuItem>
                             <Divider variant='inset' component='li' />
-                        </>
+                        </div>
                     ))
+                        :
+                        <StyledMenuItem>
+                            <ListItemText primary='You do not have any new notifications' />
+                        </StyledMenuItem>
                 }
             </StyledMenu>
         </div>
