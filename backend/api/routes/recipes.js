c2b147615ca27873fdd31b2885af0fff78e47f0f
Ivan <lukac.ivan94@gmail.com>
2020-07-17 23:46:22 +0200

    Added checkAuth withjwt

--- backend/api/routes/recipes.js
+++ backend/api/routes/recipes.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 
 const RecipeController = require('../controllers/recipes');
 
 router.get('/', RecipeController.recipes_get_all);
-router.get('/:recipeId', RecipeController.recipes_get_recipe);
-router.get('/ofuser/:userId', RecipeController.recipes_get_recipes_of_user);
-router.post('/', RecipeController.recipes_add_recipe);
-router.patch('/:recipeId', RecipeController.recipes_edit_recipe);
-router.patch('/cancel/:recipeId', RecipeController.recipes_cancel_recipe);
-router.delete('/:recipeId', RecipeController.recipes_delete_recipe);
-router.delete('/', RecipeController.recipes_delete_all);
+router.get('/:recipeId',checkAuth, RecipeController.recipes_get_recipe);
+router.get('/ofuser/:userId',checkAuth, RecipeController.recipes_get_recipes_of_user);
+router.post('/',checkAuth, RecipeController.recipes_add_recipe);
+router.patch('/:recipeId',checkAuth, RecipeController.recipes_edit_recipe);
+router.patch('/cancel/:recipeId',checkAuth, RecipeController.recipes_cancel_recipe);
+router.delete('/:recipeId',checkAuth, RecipeController.recipes_delete_recipe);
+router.delete('/',checkAuth, RecipeController.recipes_delete_all);
 
 module.exports = router;
